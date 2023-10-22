fe38e862879ca83fe9616c5721c7dc77594a8104
rikmelos <rikmelos@gmail.com>
2023-10-22 01:02:54 -0500

    se agregan dependencias del jwt y config inicial de modelo de datos

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,10 +1,33 @@
 from flask import Flask, request, render_template, redirect, url_for, session
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key  = 'your_secret_key'
 
 jwt = JWTManager(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+class FileConversionTask(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('file_conversion_tasks', lazy=True))
+    original_filename = db.Column(db.String(255), nullable=False)
+    converted_filename = db.Column(db.String(255))
+    conversion_format = db.Column(db.String(20), nullable=False)
+    status = db.Column(db.String(20), default='uploaded')
+
+    def __init__(self, user, original_filename, conversion_format):
+        self.user = user
+        self.original_filename = original_filename
+        self.conversion_format = conversion_format
 
 users = {
     'user1': 'password1',
@@ -16,6 +39,13 @@ user_tasks = {
     'user1': ['Task 1', 'Task 2'],
     'user2': ['Task 3', 'Task 4'],
 }
+
+@app.route('/create_user')
+def create_user():
+    user = User(username='example_user', password='example_password')
+    db.session.add(user)
+    db.session.commit()
+    return 'User created!'
 
 @app.route('/')
 def home():
@@ -47,7 +77,7 @@ def login():
                  additional_claims={"username": username},
             )
             session['token'] = token_de_acceso
-            
+
             return redirect(url_for('home'))
         return 'Invalid username or password'
     return render_template('login.html')
@@ -75,4 +105,9 @@ def tasks():
     return 'You are not logged in. <a href="/api/auth/login">Login</a> or <a href="/api/auth/register">Register</a>'
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(host='0.0.0.0')
+
+    creo que lo que no esta funionando es la creacion con este bloque: if __name__ == '__main__':
+        db.create_all()
+        app.run(host='0.0.0.0') la linea que creo que no funciona es esta: db.create_all()
